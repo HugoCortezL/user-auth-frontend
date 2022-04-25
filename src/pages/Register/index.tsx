0b1500ebae70ac1a244dd2481755025ce954ff33
HugoCortezL <hugocortezprog@gmail.com>
2022-04-25 17:57:18 -0300

    register page ready

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,48 @@
+import {Container, InputContainer} from './styles'
+import LoginRegisterLayout from '../../Layouts/LoginRegister'
+
+import {FaUserAlt} from 'react-icons/fa'
+import {MdEmail} from 'react-icons/md'
+import {AiFillLock} from 'react-icons/ai'
+
+export default function Register(){
+    return(
+        <LoginRegisterLayout>
+            <div>
+                <h1 className="title">Welcome</h1>
+                <p className="description">Create Account</p>
+            </div>
+            <InputContainer>
+                <div>
+                    <label>Username</label>
+                    <input type="text" className="input"/>
+                    <span>
+                        <FaUserAlt size={22} color="#33CBFB"/>
+                    </span>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" className="input"/>
+                    <span>
+                        <MdEmail size={22} color="#33CBFB"/>
+                    </span>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" className="input"/>
+                    <span>
+                        <AiFillLock size={22} color="#33CBFB"/>
+                    </span>
+                </div>
+            </InputContainer>
+            <Container>
+                <button className="btn">
+                    Register
+                </button>
+                <p className="register">
+                    Have account ? <span>Login</span>
+                </p>
+            </Container>
+        </LoginRegisterLayout>
+    )
+}
---
+++ src/pages/Register/styles.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    p.register{
+        font-size: 12px;
+        margin-top: 10px;
+        color: #EEFFFF;
+        span{
+            color: #B3E9FA;
+            text-decoration: underline;
+            cursor: pointer;
+        }
+    }
+`
+
+export const InputContainer = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    div{
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        label{
+            color: #EEFFFF;
+        }
+        & + div{
+            margin-top: 10px;
+        }
+        span{
+            position: absolute;
+            top: 36px;
+            left: 12px;
+        }
+    }
+`
